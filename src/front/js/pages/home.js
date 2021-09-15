73112194bdf4a179a1e626c03c3a602992d55d13
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-09-15 15:39:23 +0000

    style on home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,5 +6,28 @@ import "../../styles/home.scss";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
-	return <div>hello</div>;
+	return (
+		<div>
+			<div className="space" />
+			<div className="frontsummary">
+				<p>
+					Aqui va el About. Le das click en el navbar y te mueve aca. The easiest solution to register vacation
+					rentals in Condominiums, HOAs and any other multi-family development to integrate with management
+					and access control system.
+				</p>
+			</div>
+			<div className="smallspace" />
+			<div className="frontsummary">
+				Aqui va el pricing. Click en navbar y te mueve aca.Initial charge of integration with access control, plus monthly or yearly fee.
+				Please contact us for a quote.
+			</div>
+
+			<div className="smallspace" />
+			<div className="frontsummary">
+				Aqui va el contact. Click en navbar y te manda para aca. You can e-mail us at info@anemail.com or filling out the form below. TAbla de
+				bootsrap.
+			</div>
+			<div>tabla de contacto etc etc etc etc</div>
+		</div>
+	);
 };
