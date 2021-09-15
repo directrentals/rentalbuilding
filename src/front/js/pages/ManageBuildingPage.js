a94ba5827eac0944eff82e26102613927501b44b
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-09-15 00:39:05 +0000

    last committ for today

--- src/front/js/pages/ManageBuildingPage.js
+++ src/front/js/pages/ManageBuildingPage.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const ManageBuildingPage = () => {
-	return <div>Manage Building</div>;
+	return (
+		<div>
+			<div>Manage Building</div>
+			<Link to="/registerunit" className="btn-links">
+				REGISTER UNIT
+			</Link>
+			<Link to="/registertenant" className="btn-links">
+				REGISTER TENANT
+			</Link>
+		</div>
+	);
 };
