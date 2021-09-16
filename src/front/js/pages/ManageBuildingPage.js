51d72191a5a309faa6bf98e25c834ff75a70f646
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-09-16 23:43:28 +0000

    added tabs on managebuilding page

--- src/front/js/pages/ManageBuildingPage.js
+++ src/front/js/pages/ManageBuildingPage.js
@@ -4,13 +4,60 @@ import { Link } from "react-router-dom";
 export const ManageBuildingPage = () => {
 	return (
 		<div>
-			<div>Manage Building</div>
-			<Link to="/registerunit" className="btn-links">
-				REGISTER UNIT
-			</Link>
-			<Link to="/registertenant" className="btn-links">
-				REGISTER TENANT
-			</Link>
+			<div>
+				<ul className="nav nav-tabs" id="myTab" role="tablist">
+					<li className="nav-item" role="presentation">
+						<button
+							className="nav-link active"
+							id="home-tab"
+							data-bs-toggle="tab"
+							data-bs-target="#home"
+							type="button"
+							role="tab"
+							aria-controls="home"
+							aria-selected="true">
+							User info
+						</button>
+					</li>
+					<li className="nav-item" role="presentation">
+						<button
+							className="nav-link"
+							id="profile-tab"
+							data-bs-toggle="tab"
+							data-bs-target="#profile"
+							type="button"
+							role="tab"
+							aria-controls="profile"
+							aria-selected="false">
+							Register Unit
+						</button>
+					</li>
+					<li className="nav-item" role="presentation">
+						<button
+							className="nav-link"
+							id="contact-tab"
+							data-bs-toggle="tab"
+							data-bs-target="#contact"
+							type="button"
+							role="tab"
+							aria-controls="contact"
+							aria-selected="false">
+							Register Tenant
+						</button>
+					</li>
+				</ul>
+				<div className="tab-content" id="myTabContent">
+					<div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+						...
+					</div>
+					<div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+						...
+					</div>
+					<div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+						...
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
