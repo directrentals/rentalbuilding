2ceea3eeb1bec9049164e15e568ff632c4707b75
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-09-16 20:20:53 +0000

    worked on homepage

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,25 +9,48 @@ export const Home = () => {
 	return (
 		<div>
 			<div className="space" />
-			<div className="frontsummary">
-				<p>
-					Aqui va el About. Le das click en el navbar y te mueve aca. The easiest solution to register
-					vacation rentals in Condominiums, HOAs and any other multi-family development to integrate with
-					management and access control system.
-				</p>
+			<div>
+				<img className="frontlogo" src="https://i.ibb.co/nbtVNcG/lodgerlogo.png" />
 			</div>
-			<div className="smallspace" />
-			<div className="frontsummary">
-				Aqui va el pricing. Click en navbar y te mueve aca.Initial charge of integration with access control,
-				plus monthly or yearly fee. Please contact us for a quote.
-			</div>
+			<div className="container-box">
+				<div className="card border-dark mb-3 box">
+					<div className="card-header">What we do</div>
+					<div className="card-body text-dark">
+						<p className="card-text">
+							Provide the easiest solution to property owners and management staff registering vacation
+							rentals in Condominiums, HOAs, and other multi-family developments.
+						</p>
+					</div>
+				</div>
 
-			<div className="smallspace" />
-			<div className="frontsummary">
-				Aqui va el contact. Click en navbar y te manda para aca. You can e-mail us at info@anemail.com or
-				filling out the form below. TAbla de bootsrap.
+				<div className="card border-dark mb-3 box">
+					<div className="card-header">Pricing</div>
+					<div className="card-body text-dark">
+						<p className="card-text">
+							Contact us for a quote! If you are not satisfied after the first 30 days, we&apos;ll give
+							you back 100% of your initial monthly fee.
+						</p>
+					</div>
+				</div>
 			</div>
-			<div>tabla de contacto etc etc etc etc</div>
+			<form className="form">
+				<h2 className="h2">CONTACT US</h2>
+				<p type="Name:">
+					<input className="input" placeholder="Write your name here.." />
+				</p>
+				<p type="Email:">
+					<input className="input" placeholder="Let us know how to contact you back.." />
+				</p>
+				<p type="Message:">
+					<input className="input" placeholder="What would you like to tell us.." />
+				</p>
+				<button className="button">Send Message</button>
+				<div className="divcont">
+					<span className="fa fa-phone" />
+					001 1023 567
+					<span className="fa fa-envelope-o" /> contact@company.com
+				</div>
+			</form>
 		</div>
 	);
 };
