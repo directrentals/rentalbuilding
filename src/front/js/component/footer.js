51aedb012f381d4fd7616d18efca9e0463aa135e
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-09-19 01:53:48 +0000

    worked on footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center footcolor">
-		<p>GIVE US YOUR MONEY</p>
+		<p>Copyright – Lodger | All rights reserved | Terms & Conditions | Privacy Policy | Sitemap</p>
 	</footer>
 );
